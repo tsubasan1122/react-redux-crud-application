35fb735d4154297afb38dd8f10e09bda6e70c298
tsubasan1122 <tsubasan1122@gmail.com>
2018-12-14 06:22:16 +0000

    rearn props-types

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 //class App extends Component {
 //  render() {
@@ -14,7 +15,8 @@ import React from 'react';
 const App = () => {
   const profiles = [
     {name: "Taro", age: 10},
-    {name: "Hanako", age: 2}
+    {name: "Hanako", age: 2},
+    {name: "hide", age: 3}
   ]
   return(
     <div>
@@ -29,6 +31,11 @@ const App = () => {
 
 const User = (props) => {
   return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+}
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
