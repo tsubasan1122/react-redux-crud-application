dbdb29ea73c8f6d7409007afa655ab4a70d2ee07
tsubasan1122 <tsubasan1122@gmail.com>
2018-12-15 09:08:56 +0000

    implement action and action creater

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+
+export const increament = () => ({
+  type: 'INCREMENT'
+})
+
+export const decrement = () => ({
+  type: 'DECREMENT'
+})
